ed00f2eba3fbf1c13e546ed5813cfc2cb3a4ad2b
魏翔宇 <weixiangyu@homolo.com>
2018-04-26 19:44:24 +0800

    //工作队列，即一个生产者对多个消费者           循环分发、消息确认、消息持久、公平分发

---
+++ src/com/xywei/rabbitmq/NewTask.java
@@ -0,0 +1,43 @@
+package com.xywei.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Project:rabbitmq-demo
+ * File:com.xywei.rabbitmq
+ * Author:xywei
+ * Email :weixiangyu@homolo.com
+ * Copyright 2004-2018 Homolo Co., Ltd. All rights reserved.
+ */
+public class NewTask {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        String message = "8....";
+
+        // deliveryMode   1:非持久化  2 :持久化
+        AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties.Builder();
+        builder.deliveryMode(2);
+        AMQP.BasicProperties properties = builder.build();
+
+        //MessageProperties.PERSISTENT_TEXT_PLAIN  与 MessageProperties.PERSISTENT_TEXT_PLAIN 其实是等价的
+
+        channel.basicPublish("", TASK_QUEUE_NAME,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
---
+++ src/com/xywei/rabbitmq/Worker.java
@@ -0,0 +1,71 @@
+package com.xywei.rabbitmq;
+
+import com.rabbitmq.client.*;
+import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+/**
+ * Project:rabbitmq-demo
+ * File:com.xywei.rabbitmq
+ * Author:xywei
+ * Email :weixiangyu@homolo.com
+ * Copyright 2004-2018 Homolo Co., Ltd. All rights reserved.
+ */
+
+public class Worker {
+
+    private  final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork(String task) {
+        Date startDate = new Date();
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+        Date endDate = new Date();
+        System.out.println("  用时:"+(endDate.getTime()-startDate.getTime()));
+
+
+
+    }
+}
